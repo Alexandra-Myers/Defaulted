819895bc8ff2caae8bd4cfc70f8593d5ac281c13
Alexandra-Myers <bia416cat@gmail.com>
2025-01-26 17:07:14 -0500

    Update ClientboundDefaultComponentsSyncPacket.java

--- common/src/main/java/net/atlas/defaulted/networking/ClientboundDefaultComponentsSyncPacket.java
+++ common/src/main/java/net/atlas/defaulted/networking/ClientboundDefaultComponentsSyncPacket.java
@@ -1,7 +1,6 @@
 package net.atlas.defaulted.networking;
 
 import net.atlas.defaulted.component.ItemPatches;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
@@ -13,7 +12,7 @@ import java.util.ArrayList;
 
 public record ClientboundDefaultComponentsSyncPacket(ArrayList<ItemPatches> list) implements CustomPacketPayload {
     public static final Type<ClientboundDefaultComponentsSyncPacket> TYPE = new Type<>(ResourceLocation.fromNamespaceAndPath("defaulted", "update_status"));
-    public static final StreamCodec<FriendlyByteBuf, ClientboundDefaultComponentsSyncPacket> CODEC = ByteBufCodecs.collection(ArrayList::new, ItemPatches.STREAM_CODEC).map(ClientboundDefaultComponentsSyncPacket::new, ClientboundDefaultComponentsSyncPacket::list).mapStream(buf -> (RegistryFriendlyByteBuf) buf);
+    public static final StreamCodec<RegistryFriendlyByteBuf, ClientboundDefaultComponentsSyncPacket> CODEC = ByteBufCodecs.collection(ArrayList::new, ItemPatches.STREAM_CODEC).map(ClientboundDefaultComponentsSyncPacket::new, ClientboundDefaultComponentsSyncPacket::list);
 
     @Override
     public @NotNull Type<?> type() {
